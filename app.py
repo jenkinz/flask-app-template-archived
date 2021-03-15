ff475d4451eeec2e5b7c9dc3fbc623f91cee3015
Brian Jenkins <bjenkins@sandel.com>
2021-03-15 14:12:47 -0700

    Add JSON return example for GET request

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
+movie_entries = [
+    {
+        "name": "Terminator 2: Judgment Day",
+        "cast": ["Arnold Schwarzenegger", "Linda Hamilton", "Joe Morton"],
+        "genres": ["Action", "Sci-Fi"],
+    },
+    {
+        "name": "The Shawshank Redemption",
+        "cast": [
+            "Tim Robbins",
+            "Morgan Freeman",
+            "Bob Gunton",
+            "William " "Sadler",
+        ],
+        "genres": ["Drama"],
+    },
+]
+
+
 @app.route("/")
-def hello():
-    return {"hello": "world"}
+@app.route("/movies")
+def movies():
+    return jsonify(movie_entries)
 
 
 app.run()
